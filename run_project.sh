4d1cb63581b801aaa030cb4a58cc1edfdb3961a7
smit24-git <smitshah24.ss@gmail.com>
2024-08-31 17:34:10 -0400

    removed output file from run script

--- run_project.sh
+++ run_project.sh
@@ -1,17 +1,17 @@
-echo "Execution Started:" |& tee -a output.txt
-date |& tee -a output.txt
+echo "Execution Started:" 
+date 
 
 
-docker compose build |& tee -a output.txt
-docker compose up -d |& tee -a output.txt
-docker compose -f docker-compose.migration.yaml build |& tee -a output.txt
-docker compose -f docker-compose.migration.yaml up |& tee -a output.txt
-docker compose -f docker-compose.migration.yaml down |& tee -a output.txt
+sudo docker compose build 
+sudo docker compose up -d 
+sudo docker compose -f docker-compose.migration.yaml build 
+sudo docker compose -f docker-compose.migration.yaml up 
+sudo docker compose -f docker-compose.migration.yaml down 
 
-echo "Execution Finished: " |& tee -a output.txt
-date |& tee -a output.txt
+echo "Execution Finished: " 
+date 
 
-echo "-------------------------" |& tee -a output.txt
+echo "-------------------------" 
 
 # notepad output.txt
 
